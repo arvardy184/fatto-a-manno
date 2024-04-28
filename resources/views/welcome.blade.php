2094e70684f6d18612472f7d5d60e9369202d91c
zakkiislamii <mzislami05@gmail.com>
2024-04-29 00:19:41 +0700

    testing 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,7 +94,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Dok</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Laravel has wonderful documentation covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
+                                                Laravel covering every aspect of the framework. Whether you are a newcomer or have prior experience with Laravel, we recommend reading our documentation from beginning to end.
                                             </p>
                                         </div>
                                     </div>
