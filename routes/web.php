6d1291ace27dbffa7986c7e8c505649e39badb3f
zakkiislamii <mzislami05@gmail.com>
2024-05-02 17:19:13 +0700

    homepage

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
