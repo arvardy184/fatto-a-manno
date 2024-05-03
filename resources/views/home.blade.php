091e876dd75d530cc224287869bfd4d4bcfbb39c
zakkiislamii <mzislami05@gmail.com>
2024-05-03 21:11:20 +0700

    perubahan views sementara

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
     <div class="bg-white">
         {{-- best seller --}}
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
