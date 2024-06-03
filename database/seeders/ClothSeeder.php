cd0eb8c48e06ce7d01b40e51ce04cf3c66c788b8
Skiadra <riadywiguna5@gmail.com>
2024-06-03 14:22:48 +0700

    factory

--- database/seeders/ClothSeeder.php
+++ database/seeders/ClothSeeder.php
@@ -14,6 +14,43 @@ class ClothSeeder extends Seeder
      */
     public function run(): void
     {
-        $clothes = Cloth::factory()->count(10)->create();
+        // Define the possible values for each attribute
+        $types = ['Polo', 'Turtleneck', 'Kaos Polos', 'Wallet', 'Hoodie', 'Pants', 'Caps', 'Kemeja', 'Sweater'];
+        $colors = ['White', 'Black', 'Blue', 'Brown', 'Sage'];
+        $sizes = ['S', 'M', 'L', 'XL', 'XXL'];
+
+        // Generate combinations and seed the database
+        foreach ($types as $type) {
+            foreach ($colors as $color) {
+                foreach ($sizes as $size) {
+                    $name = $type . ' ' . $color;
+                    $this->addClothes($type, $name, $color, $size);
+                }
+            }
+        }
+    }
+
+    private function addClothes($type, $name, $color, $size)
+    {
+        // Check if the same clothes exist
+        $exist_clothes = Cloth::where('type', $type)
+            ->where('name', $name)
+            ->where('size', $size)
+            ->where('color', $color)
+            ->first();
+
+        $storage = Storage::first();
+
+        if ($exist_clothes) {
+
+            // Assuming $cloth and $storage are instances of Cloth and Storage models respectively
+            $exist_clothes->storages()->attach($storage, ['quantity' => random_int(10, 20)]);
+            return;
+        };
+
+        $clothes = Cloth::factory()->withType($type)->withName($name)->withColor($color)->withSize($size)->create();
+
+        // Assuming $cloth and $storage are instances of Cloth and Storage models respectively
+        $clothes->storages()->attach($storage, ['quantity' => random_int(10, 20)]);
     }
 }
