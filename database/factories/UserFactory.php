fb12ca1657bfd47054650901e96d0c36645aac2d
Skiadra <riadywiguna5@gmail.com>
2024-06-07 18:10:55 +0700

    tes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,13 +28,13 @@ class UserFactory extends Factory
         $faker = Faker::create('id_ID');
 
         return [
-            'name' => $faker->name,
-            'email' => $faker->unique()->safeEmail,
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => bcrypt('123456'), // You may use Hash::make() instead
             'address' => $faker->address,
             'number' => $faker->phoneNumber,
-            'role_id' => 0, // You may adjust this value as needed
+            'role_id' => 1, // You may adjust this value as needed
             'remember_token' => Str::random(10),
         ];
     }
